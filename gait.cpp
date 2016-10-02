0e34d48a2841d57a7240b4107cd93573d1fa155f
jajsmith <j.jaydez@gmail.com>
2016-10-02 16:48:41 -0400

    Remove binary. Add gyroY increasing check to detect correct number of heel strikes

--- gait.cpp
+++ gait.cpp
@@ -34,19 +34,38 @@ int BASELINE_GYRO_Y = 0;
 //     1. gyro.y reaches local minimum with negative value (enable: Heel Strike Detect)
 //     2. accel.x reaches local minimum with negative value
 //     3. accel.z reaches local minimum with negative value
-//     4. if (accel.z < 0 && accel.x < 0):
+//     4. if (accel.z < 0 && accel.x < 0 && gyro.y increasing):
 //     5.     Heel Strike Detected!
 //     2. gyro.y reaches local max with positive value (disable: Heel Strike Detect)
 
-
+// Determine if gyo.y is 
+bool isGyroYIncreasing(list<SensorPoint*>* points) {
+	int num = 5;
+	double sum = 0;
+	if (points->size() < 5) {
+		num = points->size();
+	}
+	list<SensorPoint*>::const_iterator it = points->begin();
+	double first = (*it)->gyroY;
+	for (int i = 0; i < num; i++) {
+		// Increase pointer
+		++it;
+		double diff = (*it)->gyroY - first;
+		sum += diff;
+	}
+	double avg = sum / num;
+	cout << "Avg: " << avg << endl;
+	if (avg > 0) return true;
+	else return false;
+}
 
 int main(int argc, char* argv[]){
 	cout << "Welcome to Gait Cycle Tracker!" << endl;
 	int heelStrikesDetected = 0;
 	string line;
 
-	list<SensorPoint*> points;
-	bool stdSet = false;
+	list<SensorPoint*> * points = new list<SensorPoint*>();
+	bool midStride = true;
 	//Local Minimums
 	SensorPoint* lmAccelX;
 	SensorPoint* lmAccelZ;
@@ -68,36 +87,38 @@ int main(int argc, char* argv[]){
 		SensorPoint* s = new SensorPoint(line);
 
 		// Set initial data point if necessary
-		if (points.empty()) {
-			points.push_front(s);
+		if (points->empty()) {
+			points->push_front(s);
 			continue;
 		}
 
 		// Set local mins
-		int lastGyroY = points.front()->gyroY;
+		int lastGyroY = points->front()->gyroY;
 		if ( (s->gyroY > lastGyroY) && (s->gyroY < BASELINE_GYRO_Y) ) {
 			if ( (lmGyroY == 0) || (lastGyroY < lmGyroY->gyroY) ) {
-				lmGyroY = points.front();
+				lmGyroY = points->front();
 				heelStrikeDetect = true;
 			}
 		}
 
-		int lastAccelZ = points.front()->accelZ;
+		int lastAccelZ = points->front()->accelZ;
 		if ( (s->accelZ > lastAccelZ) && (s->accelZ < BASELINE_ACCEL_Z) ) {
 			if ( (lmAccelZ == 0) || (lastAccelZ < lmAccelZ->accelZ) ) {
-				lmAccelZ = points.front();
+				lmAccelZ = points->front();
 			}
 		}
 
-		int lastAccelX = points.front()->accelX;
+		int lastAccelX = points->front()->accelX;
 		if ( (s->accelX > lastAccelX) && (s->accelX < BASELINE_ACCEL_X) ) {
 			if ( (lmAccelX == 0) || (lastAccelX < lmAccelX->accelX) ) {
-				lmAccelX = points.front();
+				lmAccelX = points->front();
 			}
 		}
 
 		// Look for Heel Strike
-		if ( (lmGyroY != 0) && (lmAccelZ != 0) && (lmAccelX != 0) && heelStrikeDetect ) {
+		if ( heelStrikeDetect && 
+			   (lmGyroY != 0) && (lmAccelZ != 0) && (lmAccelX != 0) && 
+			   isGyroYIncreasing(points) ) {
 			cout << "Heel Strike Detected at " << s->timestamp << "ms!" << endl;
 			++heelStrikesDetected;
 			heelStrikeDetect = false;
@@ -107,15 +128,16 @@ int main(int argc, char* argv[]){
 			lmAccelZ = 0;
 		}
 
-		points.push_front(s);
+		points->push_front(s);
 		// TODO: Cull points from list when they fade from relevance
-		//points.pop_back();
+		//points->pop_back();
 	}
 
 	cout << "Total Heel Strikes Detected: " << heelStrikesDetected << endl;
 	//Clean up list
-	while( !points.empty() ) {
-		delete points.front();
-		points.pop_front();
+	while( !points->empty() ) {
+		delete points->front();
+		points->pop_front();
 	}
+	delete points;
 }
